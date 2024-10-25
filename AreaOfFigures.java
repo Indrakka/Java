a210329c47b1ba306d4cf7cc21765ec085f292f7
Indrakka <96984424+Indrakka@users.noreply.github.com>
2024-10-25 10:22:35 +0530

    Add files via upload

---
+++ AreaOfFigures.java
@@ -0,0 +1,26 @@
+package com.edu;
+
+public class AreaOfFigures {
+
+	public static void main(String[] args) {
+//		int b=3;
+//		int h=9;
+//		float a;
+//		//a=(float)1/2*b*h; // 0.5*3*9
+//		//a=0.5f*b*h;
+//		a=(float)(b*h)/2; //27/2=13.5
+//	
+//		System.out.println("Area of Triangle "+a);
+		
+		int r=5;
+		float area;
+		
+		//area=3.14159f*r*r;
+		area = (float)22/7*r*r;
+		
+		System.out.println("Area of Circle ="+area);
+		
+		
+	}
+
+}
